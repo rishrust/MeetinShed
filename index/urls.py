3d92fe3da3dd3ba59aedec3f727b64338fc38177
Rishabh <rishabhsinghcomp@gmail.com>
2022-09-29 17:04:52 +0530

    first

--- index/urls.py
+++ index/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('login',views.login,name='login'),
     path('register',views.register,name='register'),
-    path('register/otp_verify>',views.otp_ver,name='otp_verification'),
+    path('register/otp_verify',views.otp_ver,name='otp_verification'),
     
     path('logged_in/',include("LOGIN.urls"),name="logged_in"),
     path('joinmeeting',views.joinmeeting,name='joinmeeting'),
